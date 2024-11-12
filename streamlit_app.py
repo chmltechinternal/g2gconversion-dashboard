74e5dd80880d278fbe0f299357a40bec5078bf81
chmltechinternal <chmltechinternal@gmail.com>
2024-11-12 15:39:41 +0000

    removed scikit-learn

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import numpy as np
 from scipy.special import expit
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
